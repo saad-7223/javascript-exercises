53ae4060e99f2393eed21faf6c76e597792ced8a
saad-7223 <saadsyed837@gmail.com>
2024-01-11 22:25:59 +0530

    wrote the reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let ns = "";
+    for (let i=1;i<=s.length;i++){
+        ns+=s.at(-i)
+    }
+    return ns
 };
 
 // Do not edit below this line
