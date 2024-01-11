32d4590029b85b7f5d3baa5112a5a768ff016d48
saad-7223 <saadsyed837@gmail.com>
2024-01-11 20:51:46 +0530

    completed task 02_repeatString all 7 tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(s,n) {
+    if(n!=-1){
+        let ns = ""  //empty string
+        for (let i=0;i<n;i++){
+            ns += s  //adds string to empty string
+        }
+        return ns // returns the concatenated string
+    }else{
+        return 'ERROR' // return error if number is negative
+    }
 };
 
 // Do not edit below this line
