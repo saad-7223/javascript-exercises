f06fe407bbe01f3d5f3199ec7c290ca4a7e6a1ca
saad-7223 <saadsyed837@gmail.com>
2024-01-12 01:21:53 +0530

    task completed : 05_sumAll and learnt about 'Number' datatype in js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
-};
+const sumAll = function(start,stop) {
+    if (!Number.isInteger(start) || !Number.isInteger(stop)){
+        return 'ERROR'
+    }
+    if (start < 0 || stop < 0){
+        return 'ERROR'
+    }
+    if (start > stop){
+        [start,stop] = [stop,start]
+        
+    }
+    let sm = 0;
+    for(let i=start;i<=stop;i++){
+        sm+=i;
+    }
+    return sm
+}
 
 // Do not edit below this line
 module.exports = sumAll;
