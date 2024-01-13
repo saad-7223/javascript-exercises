d376d5f6200ff4ec46927852b9a752af0b9a28c0
saad-7223 <saadsyed837@gmail.com>
2024-01-13 23:10:22 +0530

    fibonacci function that returns the value of the given position in the fibo series

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let a = 0;
+    let b = 1;
+    let c = 1; 
+    while(n>1){ // series terminating condition 
+        c = a+b; //fibonacci series generator
+        [a,b] = [b,c]; // element swapping
+        n-- // series terminator
+    }
+    return c
 };
 
 // Do not edit below this line
