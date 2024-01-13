156b5297ca2ffb125fc37dd3e088e809ddde7bbf
saad-7223 <saadsyed837@gmail.com>
2024-01-13 21:31:31 +0530

    completed all the calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+    return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+    return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	sm = 0;
+  for(let i=0;i<array.length;i++){
+    sm+=array[i];
+  }
+  return sm;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  prod = 1;
+  for(let i=0;i<array.length;i++){
+    prod*=array[i];
+  }
+  return prod;
 };
 
-const power = function() {
-	
-};
+const power = function(b,p) {
+	return b**p
+}; 
 
-const factorial = function() {
-	
+const factorial = function(n) {
+    if (n<2){
+      return 1
+    }
+    else{
+      return n*factorial(n-1)
+    }
 };
 
 // Do not edit below this line
