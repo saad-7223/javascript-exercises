833a970d55a3bb00b8ba594b56ddcb689d089649
saad-7223 <saadsyed837@gmail.com>
2024-01-13 22:39:47 +0530

    learnt about regex in js and solved the palindromes first test case

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    pattern = /[0-9a-zA-Z]/g
+    let original =s.split(" ").join("").toLowerCase()
+    let reversed = s.match(pattern).reverse().join("").toString().toLowerCase()
+    return original == reversed
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+ 
