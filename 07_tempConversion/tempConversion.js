9086944b4f543b4234765d91883bce7149c4d327
saad-7223 <saadsyed837@gmail.com>
2024-01-13 20:38:11 +0530

    fixed the formulas of both functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(temp) {
-  return Math.round((temp*9/5)+32,2)
+  return Math.round((temp-32)*9/5,2)
 };
 
 const convertToFahrenheit = function(temp) {
-  return Math.round((temp-32)*9/5,2)
+  return Math.round((temp*9/5)+32,2)
 };
 
 // Do not edit below this line
