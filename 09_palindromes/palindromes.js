aec004290a102b76aac19d74ff2a20a78a3ef72c
saad-7223 <saadsyed837@gmail.com>
2024-01-13 22:52:28 +0530

    fixed the punctuation logical error

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,7 @@
 const palindromes = function (s) {
     pattern = /[0-9a-zA-Z]/g
-    let original =s.split(" ").join("").toLowerCase()
-    let reversed = s.match(pattern).reverse().join("").toString().toLowerCase()
+    let original =s.match(pattern).join("").toLowerCase() //contains only alphanum and not any punctuations
+    let reversed = s.match(pattern).reverse().join("").toString().toLowerCase() //contains the reversed alphanum
     return original == reversed
 };
 
