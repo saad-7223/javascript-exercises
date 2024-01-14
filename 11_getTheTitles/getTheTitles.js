0b0b3f343a3cf45cb07236e2e113b3cbc8312f5b
saad-7223 <saadsyed837@gmail.com>
2024-01-14 23:39:02 +0530

    learnt about arrow function and map function in array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.map((books) => (books.title))
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+ 
