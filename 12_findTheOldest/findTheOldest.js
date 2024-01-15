804ca53d2fb03c2f52dd6fc82cf5fcc854d767aa
saad-7223 <saadsyed837@gmail.com>
2024-01-15 14:19:08 +0530

    did accomplish the task but need to improve efficiency

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  let oldest = ""
+  let oldest_age = 0
+  const now = new Date()
+  array.forEach(element => {
+    let b = element.yearOfBirth
+    let d = element.yearOfDeath
+    if(d == null || !d) d = now.getFullYear()   
+    if (oldest_age < d-b){
+      oldest = element
+      oldest_age = d-b
+    }
+  });    
+  return oldest
 };
 
 // Do not edit below this line
